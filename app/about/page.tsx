fb9fef354325978026dae9ca5047fa2c7be3c183
yalduh <yaldafereidooniii@gmail.com>
2025-08-27 13:27:03 +0330

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1 style={{ fontSize: 36, marginBottom: 12 }}>About</h1>
+      <p>Professional + personal bio goes here.</p>
+    </main>
+  );
+}
