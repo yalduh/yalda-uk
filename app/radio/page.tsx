209acb5240a59059c07bb229fda19c87c2ff2b88
yalduh <yaldafereidooniii@gmail.com>
2025-08-27 13:23:38 +0330

    Create page.tsx

---
+++ app/radio/page.tsx
@@ -0,0 +1,8 @@
+export default function RadioPage() {
+  return (
+    <main>
+      <h1 style={{ fontSize: 36, marginBottom: 12 }}>Radio</h1>
+      <p>Mixcloud sets and playlists will live here.</p>
+    </main>
+  );
+}
